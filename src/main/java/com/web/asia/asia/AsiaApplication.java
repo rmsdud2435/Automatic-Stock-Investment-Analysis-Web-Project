dc88f4c2d7d38835e3e926a8a122672ead1e76dc
gykim <rmsdud2435@naver.com>
2021-12-18 18:17:07 +0900

    init project

---
+++ src/main/java/com/web/asia/asia/AsiaApplication.java
@@ -0,0 +1,13 @@
+package com.web.asia.asia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsiaApplication.class, args);
+	}
+
+}
